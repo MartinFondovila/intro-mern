adba710d59b2df568c77e0879132e352821c7c42
Martin Fondovila <fondovila.martin@gmail.com>
2021-09-03 01:24:56 -0300

    Tutorial terminado, aplicacion funcionando

--- frontend/src/Products/components/ProductLayout.js
+++ frontend/src/Products/components/ProductLayout.js
@@ -1,18 +1,57 @@
-import React, { useState } from 'react'
-import { Modal } from 'react-bulma-components'
+import React, { useState, useEffect } from 'react'
+import { Modal, Container } from 'react-bulma-components'
 import Header from './Header'
 import AddButton from './AddButton'
 import ListProducts from './ListProducts'
 import Form from './Form'
+import { saveProduct, getProducts } from '../services'
+import Loading from './Loading'
 
 const ProductLayout = () => {
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    const [products, setProducts] = useState([])
+
+    async function loadProducts () {
+        const response = await getProducts()
+        
+        if(response.status === 200) {
+            setProducts(response.data.products)
+        }
+
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        loadProducts()
+    }, [])
+
+    const handleSubmit = async (data) => {
+        await saveProduct(data)
+        loadProducts()
+        setIsModalOpen(false)
+    }
 
     return (
-        <>
+        <Container>
             <Header title="Products app"/>
             <AddButton onClick={() => setIsModalOpen(true)}/>
-            <ListProducts/>
+            {
+                isLoading && <Loading/>
+            }
+
+            {
+                !isLoading && !products.length && (
+                    <h2 className="title has-text-centered">
+                        You don't have products
+                    </h2>
+                )
+            }
+
+            {
+                !isLoading && products.length && <ListProducts products={products}/>
+            }
+
             <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
                 <Modal.Card>
                     <Modal.Card.Header showClose={false}>
@@ -21,11 +60,11 @@ const ProductLayout = () => {
                         </Modal.Card.Title>
                     </Modal.Card.Header>
                     <Modal.Card.Body>
-                        <Form/>
+                        <Form handleSubmit={handleSubmit}/>
                     </Modal.Card.Body>
                 </Modal.Card>
             </Modal>
-        </>
+        </Container>
     )
 }
 
