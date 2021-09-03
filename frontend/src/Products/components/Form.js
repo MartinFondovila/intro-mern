41333703eadd293f758b06614d15d09ae0196bfa
Martin Fondovila <fondovila.martin@gmail.com>
2021-09-02 21:28:41 -0300

    Tutorial hasta el formulario para agregar un producto

---
+++ frontend/src/Products/components/Form.js
@@ -0,0 +1,89 @@
+import React, { useState, useRef } from 'react'
+import { Form as BulmaForm, Button } from 'react-bulma-components'
+
+const { Field, Control, Label, Input } = BulmaForm
+
+const Form = () => {
+    const [formValues, setFormValues] = useState({
+        name: '',
+        stock: '',
+        price: '',
+        description: ''
+    })
+
+    const inputFileRef = useRef()
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setFormValues({ ...formValues, [name]: value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(formValues)
+        console.log(inputFileRef.current.files)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Field>
+                <Label>Name</Label>
+                <Control>
+                    <Input 
+                        placeholder='Text input'
+                        name='name'
+                        value={formValues.name}
+                        onChange={handleChange}
+                    />
+                </Control>
+            </Field>
+            <Field>
+                <Label>Stock</Label>
+                <Control>
+                    <Input 
+                        type='number'
+                        placeholder='Text input'
+                        name='stock'
+                        value={formValues.stock}
+                        onChange={handleChange}
+                    />
+                </Control>
+            </Field>
+            <Field>
+                <Label>Price</Label>
+                <Control>
+                    <Input 
+                        type='number'
+                        placeholder='Text input'
+                        name='price'
+                        value={formValues.price}
+                        onChange={handleChange}
+                    />
+                </Control>
+            </Field>
+            <Field>
+                <Label>Description</Label>
+                <Control>
+                    <Input 
+                        placeholder='Text input'
+                        name='description'
+                        value={formValues.description}
+                        onChange={handleChange}
+                    />
+                </Control>
+            </Field>
+
+            <Field>
+                <Label>Image</Label>
+                <Control>
+                    <input type='file' ref={inputFileRef} />
+                </Control>
+            </Field>
+            <Button type='submit' color='primary'>
+                Save
+            </Button>
+        </form>
+    )
+}
+
+export default Form
--- frontend/src/Products/components/ProductLayout.js
+++ frontend/src/Products/components/ProductLayout.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal } from 'react-bulma-components'
 import Header from './Header'
 import AddButton from './AddButton'
-import Loading from './Loading'
+import ListProducts from './ListProducts'
+import Form from './Form'
 
 const ProductLayout = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
     return (
         <>
             <Header title="Products app"/>
-            <AddButton/>
-            <Loading/>
+            <AddButton onClick={() => setIsModalOpen(true)}/>
+            <ListProducts/>
+            <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <Modal.Card>
+                    <Modal.Card.Header showClose={false}>
+                        <Modal.Card.Title>
+                            Add Product
+                        </Modal.Card.Title>
+                    </Modal.Card.Header>
+                    <Modal.Card.Body>
+                        <Form/>
+                    </Modal.Card.Body>
+                </Modal.Card>
+            </Modal>
         </>
     )
 }
